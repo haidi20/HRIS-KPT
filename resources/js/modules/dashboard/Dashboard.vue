905b7446f67b6c83e9ee3e1c7982a66e970e1f06
haidi20 <nhaidii@yahoo.com>
2023-04-07 13:20:34 +0800

    setup project

---
+++ resources/js/modules/dashboard/Dashboard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Dashboard</h1>
+    <br />
+    <b-button v-b-modal.modal-1>Launch demo modal</b-button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ resources/js/stores/main.js
@@ -0,0 +1,50 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import axios from "axios";
+import moment from "moment";
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+    modules: {
+        // roster: Roster,
+    },
+    state: {
+        user: {},
+        permissions: [],
+        base_url: null,
+        name_menu: null,
+        permission: {
+            is_edit: true,
+            is_delete: true,
+        },
+    },
+    mutations: {
+        INSERT_BASE_URL(state, payload) {
+            state.base_url = payload.base_url;
+        },
+        INSERT_USER(state, payload) {
+            state.user = payload.user;
+        },
+        INSERT_NAME_MENU(state, payload) {
+            state.name_menu = payload.name_menu;
+        },
+        UPDATE_PERMISSION_IS_EDIT(state, payload) {
+            state.permission.is_edit = payload.value;
+        },
+        UPDATE_PERMISSION_IS_DELETE(state, payload) {
+            state.permission.is_delete = payload.value;
+        },
+        INSERT_PERMISSIONS(state, payload) {
+            state.permissions = payload.permissions;
+        },
+    },
+    actions: {
+        //
+    },
+    getters: {
+        //
+    }
+})
+
+export default store;
