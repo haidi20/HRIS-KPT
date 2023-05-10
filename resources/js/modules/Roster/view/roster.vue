1f4e2a9d71e9c2f2b2e2be6a03bba50c20f583f8
haidi20 <haidinurhadinata@gmail.com>
2023-05-10 11:28:46 +0800

    kasbon: update tampilan form

--- resources/js/modules/Roster/view/roster.vue
+++ resources/js/modules/Roster/view/roster.vue
@@ -19,9 +19,9 @@
           <b-tab title="Utama">
             <Main />
           </b-tab>
-          <b-tab title="Total">
+          <!-- <b-tab title="Total">
             <Total />
-          </b-tab>
+          </b-tab>-->
           <b-tab title="Status">
             <Status />
           </b-tab>
