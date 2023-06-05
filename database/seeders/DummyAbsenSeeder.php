9c8a407c76b95fefee226bc06eb9113c1ce10e69
mnuralfiansyah <mnuralfiansyah@gmail.com>
2023-06-05 14:12:39 +0800

    ':hammer:'

---
+++ database/seeders/DummyAbsenSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DummyAbsenSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
