7d03dcf15feec8022e47d0efd8608f6ede82f6cb
haidi20 <haidinurhadinata@gmail.com>
2023-06-05 19:35:08 +0800

    absen: berhasil dapat jam awal dan selesai istirahat

--- database/sqls/views/vw_attendance.sql
+++ database/sqls/views/vw_attendance.sql
@@ -4,67 +4,43 @@ DROP VIEW IF EXISTS `VW_ATTENDANCE`;
 
 CREATE VIEW VW_ATTENDANCE AS 
 	SELECT
-	    DATE_FORMAT(af.scan_date, "%Y-%m-%d") AS "date", (
-	        CASE
-	            WHEN (
-	                DATE_FORMAT(af.scan_date, "%H:%i") >= DATE_FORMAT(wh.start_time, "%H:%i") && DATE_FORMAT(af.scan_date, "%H:%i") <= DATE_FORMAT(wh.start_rest, "%H:%i")
-	            ) THEN DATE_FORMAT(af.scan_date, "%H:%i")
-	            ELSE NULL
-	        END
-	    ) AS hour_start, (
-	        CASE
-	            WHEN (
-	                DATE_FORMAT(af.scan_date, "%H:%i") >= DATE_FORMAT(wh.fastest_time, "%H:%i") && DATE_FORMAT(af.scan_date, "%H:%i") <= DATE_FORMAT(wh.after_work_end, "%H:%i")
-	            ) THEN DATE_FORMAT(af.scan_date, "%H:%i")
-	            ELSE NULL
-	        END
-	    ) AS hour_end, (
-	        CASE
-	            WHEN (
-	                DATE_FORMAT(af.scan_date, "%H:%i") >= DATE_FORMAT(wh.start_rest, "%H:%i") && DATE_FORMAT(af.scan_date, "%H:%i") <= DATE_FORMAT(wh.end_rest, "%H:%i")
-	            ) THEN DATE_FORMAT(af.scan_date, "%H:%i")
-	            ELSE NULL
-	        END
-	    ) AS hour_rest_start, (
-	        CASE
-	            WHEN (
-	                DATE_FORMAT(af.scan_date, "%H:%i") >= DATE_FORMAT(wh.start_rest, "%H:%i") && DATE_FORMAT(af.scan_date, "%H:%i") <= DATE_FORMAT(wh.end_rest, "%H:%i")
-	            ) THEN DATE_FORMAT(af.scan_date, "%H:%i")
-	            ELSE NULL
-	        END
-	    ) AS hour_rest_end,
-	    DATE_FORMAT(jt.datetime_start, "%H:%i") AS hour_overtime_job_order_start,
-	    DATE_FORMAT(jt.datetime_end, "%H:%i") AS hour_overtime_job_order_end,
-	    DATE_FORMAT(jt.datetime_start, "%Y-%m-%d") AS jt_date_overtime_start,
-	    DATE_FORMAT(jt.datetime_end, "%Y-%m-%d") AS jt_date_overtime_end,
+	    DATE_FORMAT(af.scan_date, "%Y-%m-%d") AS "date",
 	    af.pin,
-	    fi.id_finger,
-	    em.name as employee_name,
-	    fi.employee_id
+	    af_hour_rest.hour_rest_start,
+	    af_hour_rest.hour_rest_end,
+	    af_hour_rest.date as af_hour_rest_date
 	FROM
 	    attendance_fingerspots af
-	    LEFT JOIN fingers fi ON af.pin = fi.id_finger
-	    LEFT JOIN employees em ON fi.employee_id = em.id
-	    LEFT JOIN job_status_has_parents jt ON fi.employee_id = jt.employee_id
-	    AND DATE_FORMAT(jt.datetime_start, "%Y-%m-%d") = DATE_FORMAT(af.scan_date, "%Y-%m-%d")
 	    LEFT JOIN (
-	        SELECT *
-	        FROM working_hours
-	        LIMIT 1
-	    ) as wh ON 1 = 1
-	WHERE
-	    fi.employee_id IS NOT NULL
+	        SELECT
+	            af.pin,
+	            DATE_FORMAT(af.scan_date, "%Y-%m-%d") AS "date",
+	            DATE_FORMAT(min(af.scan_date), "%H:%i") as hour_rest_start, (
+	                CASE
+	                    WHEN max(af.scan_date) = min(af.scan_date) THEN NULL
+	                    ELSE DATE_FORMAT(max(af.scan_date), "%H:%i")
+	                END
+	            ) as hour_rest_end
+	        FROM
+	            attendance_fingerspots af
+	            LEFT JOIN (
+	                SELECT *
+	                FROM
+	                    working_hours
+	                LIMIT
+	                    1
+	            ) as wh ON 1 = 1
+	        WHERE
+	            DATE_FORMAT(af.scan_date, "%H:%i") >= DATE_FORMAT(wh.start_rest, "%H:%i")
+	            AND DATE_FORMAT(af.scan_date, "%H:%i") <= DATE_FORMAT(wh.end_rest, "%H:%i")
+	        GROUP BY
+	            af.pin,
+	            date
+	    ) AS af_hour_rest ON af.pin = af_hour_rest.pin
+	    AND DATE_FORMAT(af.scan_date, "%Y-%m-%d") = af_hour_rest.date
 	GROUP BY
+	    DATE_FORMAT(af.scan_date, "%Y-%m-%d"),
 	    af.pin,
-	    fi.id_finger,
-	    hour_start,
-	    hour_end,
-	    hour_rest_start,
-	    hour_overtime_job_order_start,
-	    hour_overtime_job_order_end,
-	    jt_date_overtime_start,
-	    jt_date_overtime_end,
-	    date,
-	    fi.employee_id
-	ORDER BY date
-DESC; 
+	    af_hour_rest.hour_rest_start,
+	    af_hour_rest.hour_rest_end,
+	    af_hour_rest_date
