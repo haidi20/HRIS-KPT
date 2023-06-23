cc8933ff79d4ed232dcdf39d87c473d482a303cf
haidi20 <haidinurhadinata@gmail.com>
2023-06-23 13:25:15 +0800

    project: test update js

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js').vue()
+mix.options({ runtimeChunkPath: '.' });
+
+mix.js('resources/js/app.js', 'public/js').vue({ version: 2 })
     .sass('resources/sass/app.scss', 'public/css')
     .sourceMaps();
