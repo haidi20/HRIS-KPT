06f364c105efcba91f0b256961ff96242a57de6b
haidi20 <haidinurhadinata@gmail.com>
2023-07-13 09:30:20 +0800

    dashboard, berhasil tampilkan data total ke tampilan

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\AttendanceHasEmployee;
 use App\Models\Employee;
+use App\Models\WorkingHour;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
@@ -30,6 +31,7 @@ class DashboardController extends Controller
     {
         $dateNow = Carbon::now()->format("Y-m-11");
 
+        $workingHour = WorkingHour::first();
         $totalEmployee = Employee::active()->count();
         $queryEmployeeNotCombackAfterRest = AttendanceHasEmployee::whereDate("hour_rest_start", $dateNow)
             ->whereNull("hour_rest_end")
@@ -39,32 +41,47 @@ class DashboardController extends Controller
             ->whereNotNull("hour_start")
             ->distinct("employee_id");
 
+        $hourMaxLatePresent = Carbon::parse($workingHour->start_time)->addMinutes(90)->format("H:i");
+        $queryEmployeeAbsenceLate = clone $queryEmployeeAbsence;
+        $queryEmployeeAbsenceLate = $queryEmployeeAbsenceLate->whereTime("hour_start", ">=", $hourMaxLatePresent);
+
         $dataNotCombackAfterRests = $queryEmployeeNotCombackAfterRest->get();
         $totalNotCombackAfterRest = $queryEmployeeNotCombackAfterRest->count();
 
-        $dataEmployeeAbsences = $queryEmployeeAbsence
-            ->pluck("employee_id");
+        $dataEmployeeIdAbsences = $queryEmployeeAbsence->pluck("employee_id");
+        $dataEmployeeAbsences = $queryEmployeeAbsence->get();
         $totalEmployeeAbsence = $queryEmployeeAbsence->count();
 
+        // start not absence
         $queryEmployeeNotAbsence = Employee::active()
             ->select("id", "name", "position_id")
-            ->whereNotIn("id", $dataEmployeeAbsences);
+            ->whereNotIn("id", $dataEmployeeIdAbsences);
 
         $dataEmployeeNotAbsences = $queryEmployeeNotAbsence->get();
         $totalEmployeeNotAbsence = $queryEmployeeNotAbsence->count();
+        // end not absence
+
+        // start late present
+        $dataEmployeeAbsenceLate = $queryEmployeeAbsenceLate->get();
+        $totalEmployeeAbsenceLate = $queryEmployeeAbsenceLate->count();
+        // end late present
+
 
         return response()->json([
             "success" => true,
             "dateNow" => $dateNow,
+            "hourMaxLatePresent" => $hourMaxLatePresent,
             // total
             "totalEmployee" => $totalEmployee,
             "totalEmployeeAbsence" => $totalEmployeeAbsence,
             "totalNotCombackAfterRest" => $totalNotCombackAfterRest,
             "totalEmployeeNotAbsence" => $totalEmployeeNotAbsence,
+            "totalEmployeeAbsenceLate" => $totalEmployeeAbsenceLate,
             // data
             "dataEmployeeAbsences" => $dataEmployeeAbsences,
             "dataEmployeeNotAbsences" => $dataEmployeeNotAbsences,
             "dataNotCombackAfterRests" => $dataNotCombackAfterRests,
+            "dataEmployeeAbsenceLate" => $dataEmployeeAbsenceLate,
         ]);
     }
 
--- resources/js/stores/Dashboard.js
+++ resources/js/stores/Dashboard.js
@@ -5,6 +5,52 @@ const defaultForm = {
     id: null,
 }
 
+const defaultTotal = [
+    {
+        field: "employee",
+        title: "Karyawan",
+        value: 1,
+        color: "pink",
+        //   icon: "fas fa-chalkboard-teacher",
+        icon: "fas fa-user",
+        data: [],
+    },
+    {
+        field: "absence",
+        title: "Absen Masuk",
+        value: 5,
+        color: "green",
+        //   icon: "fas fa-cocktail",
+        icon: "fas fa-user",
+        data: [],
+    },
+    {
+        field: "notAbsence",
+        title: "Belum Absen Datang ",
+        value: 5,
+        color: "red",
+        icon: "fas fa-user",
+        data: [],
+    },
+    {
+        field: "absenceLate",
+        title: "Terlambat",
+        value: 5,
+        color: "purple",
+        icon: "fas fa-user",
+        data: [],
+    },
+    {
+        field: "notCombackAfterRest",
+        title: "Belum Kembali Istirahat",
+        value: 5,
+        color: "blue",
+        //   icon: "fas fa-cocktail",
+        icon: "fas fa-user",
+        data: [],
+    },
+];
+
 const Dashboard = {
     namespaced: true,
     state: {
@@ -13,6 +59,7 @@ const Dashboard = {
             positions: [],
             employee_not_have_job_orders: [],
             five_employee_highest_job_orders: [],
+            total: [],
         },
         params: {
             month: new Date(),
@@ -31,10 +78,25 @@ const Dashboard = {
         INSERT_BASE_URL(state, payload) {
             state.base_url = payload.base_url;
         },
-        INSERT_TABLE(state, payload) {
+        INSERT_TABLE_ALL(state, payload) {
             state.data.positions = payload.positions;
             state.data.employee_not_have_job_orders = payload.employee_not_have_job_orders;
             state.data.five_employee_highest_job_orders = payload.five_employee_highest_job_orders;
+        },
+        INSERT_DATA_TOTAL(state, payload) {
+            state.data.total = [...defaultTotal];
+        },
+        INSERT_DATA_TOTAL_VALUE(state, payload) {
+            // console.info(payload);
+            const getIndex = state.data.total.findIndex(item => item.field == payload.field);
+
+            state.data.total[getIndex] = {
+                ...state.data.total[getIndex],
+                value: payload.value,
+                data: payload.data.length > 0 ? payload.data : [],
+            }
+
+            // console.info(state.data.total);
         },
         INSERT_FORM(state, payload) {
             state.form = { ...state.form, ...payload.form };
@@ -66,12 +128,35 @@ const Dashboard = {
                     params: { ...params },
                 })
                 .then((responses) => {
-                    console.info(responses);
+                    // console.info(responses);
                     const data = responses.data;
 
-                    // context.commit("INSERT_TABLE", {
-                    //     total: data.total,
-                    // });
+                    context.commit("INSERT_DATA_TOTAL");
+                    context.commit("INSERT_DATA_TOTAL_VALUE", {
+                        field: "employee",
+                        value: data.totalEmployee,
+                        data: [],
+                    });
+                    context.commit("INSERT_DATA_TOTAL_VALUE", {
+                        field: "absence",
+                        value: data.totalEmployeeAbsence,
+                        data: data.dataEmployeeAbsences,
+                    });
+                    context.commit("INSERT_DATA_TOTAL_VALUE", {
+                        field: "notCombackAfterRest",
+                        value: data.totalNotCombackAfterRest,
+                        data: data.dataNotCombackAfterRests,
+                    });
+                    context.commit("INSERT_DATA_TOTAL_VALUE", {
+                        field: "notAbsence",
+                        value: data.totalEmployeeNotAbsence,
+                        data: data.dataEmployeeNotAbsences,
+                    });
+                    context.commit("INSERT_DATA_TOTAL_VALUE", {
+                        field: "absenceLate",
+                        value: data.totalEmployeeAbsenceLate,
+                        data: data.dataEmployeeAbsenceLate,
+                    });
                     // context.commit("UPDATE_LOADING_TABLE", { value: false });
                 })
                 .catch((err) => {
