64d0137be40fad79638ceca4a8b470d2410e3b56
haidi20 <haidinurhadinata@gmail.com>
2023-07-14 15:16:30 +0800

    update merge

--- resources/js/modules/dashboard/View/dashboard.vue
+++ resources/js/modules/dashboard/View/dashboard.vue
@@ -5,6 +5,7 @@
         <div class="row">
           <div class="col-12 col-md-6 order-md-1 order-last d-flex align-items-center">
             <h3 class="d-flex align-items-center">Dashboard</h3>
+            <span>&nbsp;v1.1</span>
           </div>
         </div>
       </div>
@@ -13,8 +14,38 @@
       <section class="row">
         <div class="col-12 col-lg-12">
           <div class="row">
-            <div class="col-6 col-lg-3 col-md-6" v-for="(total, index) in list_total" :key="index">
-              <div class="card">
+            <!-- <template v-if="getDataTotal.length > 0">
+              <div
+                :class="`col-6 col-md-6 col-lg-3`"
+                v-for="(total, index) in getDataTotal"
+                :key="index"
+              >
+                <div class="card cursor-pointer" @click="onShowData(total)">
+                  <div class="card-body py-3-5">
+                    <div class="row">
+                      <div class="col-md-4">
+                        <div :class="`stats-icon ${total.color}`">
+                          <i :class="total.icon"></i>
+                        </div>
+                      </div>
+                      <div class="col-md-8">
+                        <h6 class="text-muted font-semibold">{{total.title}}</h6>
+                        <h6 class="font-extrabold mb-0">{{total.value}}</h6>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </template>
+            <template v-else>
+              <span style="margin-bottom: 20px">Loading...</span>
+            </template>-->
+            <div
+              :class="`col-6 col-md-6 col-lg-3`"
+              v-for="(total, index) in getDataTotal"
+              :key="index"
+            >
+              <div class="card cursor-pointer" @click="onShowData(total)">
                 <div class="card-body py-3-5">
                   <div class="row">
                     <div class="col-md-4">
@@ -46,7 +77,10 @@
                     bordered
                   >
                     <template v-slot:filter>
-                      <b-col cols>Karyawan Belum Punya Job Order</b-col>
+                      <b-col cols @click="onShowSettingPosition" class="cursor-pointer">
+                        Karyawan Belum Punya Job Order
+                        <!-- <i class="fas fa-cogs"></i> -->
+                      </b-col>
                     </template>
                     <template v-slot:tbody="{ filteredData }">
                       <b-tr v-for="(item, index) in filteredData" :key="index">
@@ -107,7 +141,7 @@
                     <template v-slot:tbody="{ filteredData }">
                       <b-tr v-for="(item, index) in filteredData" :key="index">
                         <b-td
-                          v-for="column in getColumns()"
+                          v-for="column in getColumns('position_columns')"
                           :key="column.label"
                         >{{ item[column.field] }}</b-td>
                       </b-tr>
@@ -120,6 +154,97 @@
         </div>
       </section>
     </div>
+    <b-modal
+      id="data_total"
+      ref="data_total"
+      title="Data Karyawan"
+      size="md"
+      class="modal-custom"
+      hide-footer
+    >
+      <b-row>
+        <b-col cols>
+          <DatatableClient
+            :data="getDataSelecteds"
+            :columns="employee_notyet_columns"
+            :options="options"
+            nameStore="dashboard"
+            nameLoading="selected"
+            :filter="false"
+            :footer="false"
+            bordered
+          >
+            <template v-slot:tbody="{ filteredData }">
+              <b-tr v-for="(item, index) in filteredData" :key="index">
+                <b-td>{{item.name != undefined ? item.name : item.employee_name}}</b-td>
+                <b-td>{{item.position_name}}</b-td>
+              </b-tr>
+            </template>
+          </DatatableClient>
+        </b-col>
+      </b-row>
+      <br />
+      <b-row>
+        <b-col style="text-align: -webkit-right;">
+          <b-button variant="info" @click="onCloseModal()">Tutup</b-button>
+        </b-col>
+      </b-row>
+    </b-modal>
+    <b-modal
+      id="data_setting_position"
+      ref="data_setting_position"
+      title="Jabatan yang menggunakan job order"
+      size="md"
+      class="modal-custom"
+      hide-footer
+    >
+      <b-row>
+        <b-col cols>
+          <b-form-group label label-for="position_id" class>
+            <VueSelect
+              id="position_id"
+              class="cursor-pointer"
+              v-model="form.position_id"
+              placeholder="Pilih Jabatan"
+              :options="getOptionPositions"
+              :reduce="(data) => data.id"
+              label="name"
+              searchable
+              style="min-width: 180px"
+            />
+          </b-form-group>
+        </b-col>
+        <b-col cols="3" style="align-self: center;">
+          <b-button variant="success" @click="onSend()">Tambah</b-button>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col cols>
+          <DatatableClient
+            :data="getDataDashboardHasPositions"
+            :columns="setting_position_columns"
+            :options="options"
+            nameStore="dashboard"
+            nameLoading="setting_position"
+            :filter="false"
+            :footer="false"
+            bordered
+          >
+            <template v-slot:tbody="{ filteredData }">
+              <b-tr v-for="(item, index) in filteredData" :key="index">
+                <b-td>{{item.position_name}}</b-td>
+              </b-tr>
+            </template>
+          </DatatableClient>
+        </b-col>
+      </b-row>
+      <br />
+      <b-row>
+        <b-col style="text-align: -webkit-right;">
+          <b-button variant="info" @click="onCloseModal()">Tutup</b-button>
+        </b-col>
+      </b-row>
+    </b-modal>
   </div>
 </template>
 
